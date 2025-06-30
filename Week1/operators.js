ddb81f2a5beac1e5920b171c0a02dc63f95b1873
Namutebi-shadiah <snamutebi@student.refactory.academy>
2025-06-30 16:53:59 +0300

    added

---
+++ Week1/operators.js
@@ -0,0 +1,22 @@
+// Assignment operator
+let firstName = "vian"
+x = y
+x = x + y
+x += y
+const chickPrice = 1650;
+let numberChicks = 20;
+
+
+// logic operators
+
+// And &&
+// or ||
+//not !
+// operators
++ // addition
+- // sub 
+// 
+const chicPrice = 1650
+let  numChicks = 30
+let totaPrice = chicPrice * numChicksPrice
+console.log(totaPrice);
